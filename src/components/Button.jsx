9cb449cd64893e01d5167ee63abdc0fa558fa2b3
Tornio2 <ntornev@gmail.com>
2025-06-26 19:23:28 +0300

    Add Button component styles in Button.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import '../styles/Button.css'; 
 
 function Button({ label}) {
   return (
-    <button onClick={ () => console.log(`Button clicked: ${label}`)} className="button">
+    <button onClick={ () => console.log(`Button clicked: ${label}`)} className="test-btn">
       {label}
     </button>
   );
